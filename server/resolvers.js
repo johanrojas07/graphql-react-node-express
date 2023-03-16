c7347019d0977736ce0169c05e70a3aafafd9221
Johan Esneyder Rojas Latorre <jerojas@ifxcorp.com>
2023-03-16 11:39:56 -0500

    Update and Delete Mutation

--- server/resolvers.js
+++ server/resolvers.js
@@ -8,7 +8,9 @@ export const resolvers = {
     },
 
     Mutation: {
-        createJob: (_root, {input}) => Job.create(input)
+        createJob: (_root, {input}) => Job.create(input),
+        deleteJob: (_root, {id}) => Job.delete(id),
+        updateJob: (_root, {input}) => Job.update(input)
     },
 
     Company: {
